68aea5abfaf1081a6a5c732ea7a5c1ebc9b722e1
anenglearner <an.englearner@gmail.com>
2019-11-24 10:31:25 +0900

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //index.js
 var Twitter = require('twitter')
 var twitter= new  Twitter({
-  consumer_key:'zMsBbF55dsMgBgI8NczgWaauT',
-  consumer_secret:'1f0u7oJ3mnRgL9W0RMUzltO4MLAMtva1HagnCYItOLoKustIaJ',
-  access_token_key:'717125061644124160-1qaIvhLZhO8dxcBMKCDrLPJixf65gmA',
-  access_token_secret:'vS66YeB5Dnd7E2Iw2zdkKg14TvcF0KLBT8U2yiS3rVK9R'
+  consumer_key:process.env['CONSUMER_KEY'],
+  consumer_secret:process.env['CONSUMER_SECRET'],
+  access_token_key:process.env['ACCESS_TOKEN_KEY'],
+  access_token_secret:process.env['ACCESS_TOKEN_SECRET']
 })
 
 exports.handler=function(){
